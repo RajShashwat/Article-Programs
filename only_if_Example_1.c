6c69de6a30dedecd9062c7b103c015672e847b22
Shashwat Raj <46080469+RajShashwat@users.noreply.github.com>
2019-03-03 15:56:32 +0530

    A  program to check is the entered character x?

---
+++ only_if_Example_1.c
@@ -0,0 +1,13 @@
+//Created by Shashwat Raj
+//A program to check entered alphabet is x
+#include<stdio.h>
+void main()
+{
+	char ch;
+  printf(Enter a smaller case alphabet");
+  scanf("%c",ch);
+  if(ch=='x')
+  {
+    printf("Alphabhet is x")
+  }
+}
