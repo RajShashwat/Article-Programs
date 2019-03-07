d534eeaa26de0545b4d75ac76d09e19b9e4add39
Shashwat Raj <46080469+RajShashwat@users.noreply.github.com>
2019-03-07 23:28:17 +0530

    A program to check entered alphabet is x or not

---
+++ if_else_Example_2.c
@@ -0,0 +1,13 @@
+//Created by Shashwat Raj
+//A program to check entered alphabet is x or not
+#include<stdio.h>
+void main()
+{
+	char ch;
+  printf("Enter a smaller case alphabet: ");
+  scanf("%c",&ch);
+  if(ch=='x')
+    printf("Alphabhet is x");
+  else
+    printf("Alphabet is not x");
+}
