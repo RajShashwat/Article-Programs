f863c34cbda181dfb2bb806ccc4b2840ec367fe2
Shashwat Raj <46080469+RajShashwat@users.noreply.github.com>
2019-03-07 23:09:20 +0530

    check lowercase alphabet is x

--- only_if_Example_1.c
+++ only_if_Example_1.c
@@ -4,10 +4,10 @@
 void main()
 {
 	char ch;
-  printf("Enter a smaller case alphabet");
-  scanf("%c",ch);
+  printf("Enter a smaller case alphabet: ");
+  scanf("%c",&ch);
   if(ch=='x')
   {
-    printf("Alphabhet is x")
+    printf("Alphabhet is x");
   }
 }
